70c4fa3fa9c09d09c54bc38d933991789d2a40c6
sunxian <sunxian@wintelia.com>
2023-02-23 00:10:36 +0800

    初始化代码仓库

---
+++ pages/tabbar/tabbar-1/tabbar-1.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="box">
+	<u-button text="按钮"></u-button>  
+	</view> 
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	onLoad() {},
+	methods: {} 
+};
+</script>
+
+<style lang="scss" scoped>
+	.box{
+		
+	}
+</style>
